2ae63961450e92d0e759e25f9b6cb22f0a033781
Sehab Veljacic <abivelj@gmail.com>
2024-04-17 19:58:55 -0500

    fixjodit

--- webenology.blazor.components.JoditEditor/wwwroot/jodit.editor.js
+++ webenology.blazor.components.JoditEditor/wwwroot/jodit.editor.js
@@ -13,51 +13,53 @@ let editor;
 
 export function Setup(id, mergeTags) {
     console.log(mergeTags);
-    scriptEle.addEventListener("load", () => {
-        let buttons = ['undo', 'redo', '|',
-            'bold', 'italic', 'underline', '|',
-            'align', 'font', 'fontsize', '|',
-            'ul', 'ol', '|',
-            'table', 'link', 'image'];
+
+    var interval = setInterval(() => {
+        if (Jodit != null) {
+
+            let buttons = ['undo', 'redo', '|',
+                'bold', 'italic', 'underline', '|',
+                'align', 'font', 'fontsize', '|',
+                'ul', 'ol', '|',
+                'table', 'link', 'image'];
 
 
-        if (mergeTags) {
-            Jodit.defaultOptions.controls.mergeTags = {
-                icon: '<svg width="800px" height="800px" viewBox="0 0 16 16" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">' +
-                    '<path fill="#444" d="M2.1 3.1c0.2 1.3 0.4 1.6 0.4 2.9 0 0.8-1.5 1.5-1.5 1.5v1c0 0 1.5 0.7 1.5 1.5 0 1.3-0.2 1.6-0.4 2.9-0.3 2.1 0.8 3.1 1.8 3.1s2.1 0 2.1 0v-2c0 0-1.8 0.2-1.8-1 0-0.9 0.2-0.9 0.4-2.9 0.1-0.9-0.5-1.6-1.1-2.1 0.6-0.5 1.2-1.1 1.1-2-0.3-2-0.4-2-0.4-2.9 0-1.2 1.8-1.1 1.8-1.1v-2c0 0-1 0-2.1 0s-2.1 1-1.8 3.1z"></path>' +
-                    '<path fill="#444" d="M13.9 3.1c-0.2 1.3-0.4 1.6-0.4 2.9 0 0.8 1.5 1.5 1.5 1.5v1c0 0-1.5 0.7-1.5 1.5 0 1.3 0.2 1.6 0.4 2.9 0.3 2.1-0.8 3.1-1.8 3.1s-2.1 0-2.1 0v-2c0 0 1.8 0.2 1.8-1 0-0.9-0.2-0.9-0.4-2.9-0.1-0.9 0.5-1.6 1.1-2.1-0.6-0.5-1.2-1.1-1.1-2 0.2-2 0.4-2 0.4-2.9 0-1.2-1.8-1.1-1.8-1.1v-2c0 0 1 0 2.1 0s2.1 1 1.8 3.1z"></path>' +
-                    '</svg>',
-                list: mergeTags,
-                childTemplate: (editor, key, value) =>
-                    `<span class="${key}">${editor.i18n(value)}</span>`,
+            if (mergeTags) {
+                Jodit.defaultOptions.controls.mergeTags = {
+                    icon: '<svg width="800px" height="800px" viewBox="0 0 16 16" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">' +
+                        '<path fill="#444" d="M2.1 3.1c0.2 1.3 0.4 1.6 0.4 2.9 0 0.8-1.5 1.5-1.5 1.5v1c0 0 1.5 0.7 1.5 1.5 0 1.3-0.2 1.6-0.4 2.9-0.3 2.1 0.8 3.1 1.8 3.1s2.1 0 2.1 0v-2c0 0-1.8 0.2-1.8-1 0-0.9 0.2-0.9 0.4-2.9 0.1-0.9-0.5-1.6-1.1-2.1 0.6-0.5 1.2-1.1 1.1-2-0.3-2-0.4-2-0.4-2.9 0-1.2 1.8-1.1 1.8-1.1v-2c0 0-1 0-2.1 0s-2.1 1-1.8 3.1z"></path>' +
+                        '<path fill="#444" d="M13.9 3.1c-0.2 1.3-0.4 1.6-0.4 2.9 0 0.8 1.5 1.5 1.5 1.5v1c0 0-1.5 0.7-1.5 1.5 0 1.3 0.2 1.6 0.4 2.9 0.3 2.1-0.8 3.1-1.8 3.1s-2.1 0-2.1 0v-2c0 0 1.8 0.2 1.8-1 0-0.9-0.2-0.9-0.4-2.9-0.1-0.9 0.5-1.6 1.1-2.1-0.6-0.5-1.2-1.1-1.1-2 0.2-2 0.4-2 0.4-2.9 0-1.2-1.8-1.1-1.8-1.1v-2c0 0 1 0 2.1 0s2.1 1 1.8 3.1z"></path>' +
+                        '</svg>',
+                    list: mergeTags,
+                    childTemplate: (editor, key, value) =>
+                        `<span class="${key}">${editor.i18n(value)}</span>`,
 
-                exec(editor, _, { control }) {
-                    if (control.name == "mergeTags")
+                    exec(editor, _, { control }) {
+                        if (control.name == "mergeTags")
+                            return false;
+                        let key = control.args && control.args[0];
+                        let value = control.args && control.args[1];
+
+                        let node = document.createElement("span");
+                        node.innerHTML = `${key}`;
+
+                        node.contentEditable = false;
+                        node.dataset.value = key;
+                        node.classList.add("merge-tag");
+                        node.addEventListener("click", function () {
+                            editor.s.removeNode(this);
+                        });
+                        // editor.s.insertHTML(str);
+                        editor.s.insertNode(node, true, true);
+
+                        editor.setEditorValue(); // Synchronizing the state of the WYSIWYG editor and the source textarea
+
                         return false;
-                    let key = control.args && control.args[0];
-                    let value = control.args && control.args[1];
-
-                    let node = document.createElement("span");
-                    node.innerHTML = `${key}`;
-
-                    node.contentEditable = false;
-                    node.dataset.value = key;
-                    node.classList.add("merge-tag");
-                    node.addEventListener("click", function () {
-                        editor.s.removeNode(this);
-                    });
-                    // editor.s.insertHTML(str);
-                    editor.s.insertNode(node, true, true);
-
-                    editor.setEditorValue(); // Synchronizing the state of the WYSIWYG editor and the source textarea
-
-                    return false;
+                    }
                 }
+                buttons.push('mergeTags');
             }
-            buttons.push('mergeTags');
-        }
 
-        var interval = setInterval(() => {
             if (document.getElementById(id) != null) {
                 var idDoc = "#" + id;
                 console.log("editor id", idDoc);
@@ -85,9 +87,8 @@ export function Setup(id, mergeTags) {
                     }
                 }
             }
-        }, 500);
-
-    });
+        }
+    }, 500);
 }
 
 export function GetText() {
