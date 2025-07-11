851ba465724b6d9d1b5b603aabb9f8afdea9fba3
Sehab Veljacic <abivelj@gmail.com>
2025-07-11 13:21:59 -0500

    updated receipt

--- webenology.blazor.components.shared/SearchHelper.cs
+++ webenology.blazor.components.shared/SearchHelper.cs
@@ -1,10 +1,70 @@
-﻿using System.Text;
+﻿using System.Net.Http.Headers;
+using System.Text;
 using System.Text.RegularExpressions;
+using System.Xml;
+
+using Microsoft.VisualBasic;
 
 namespace webenology.blazor.components.shared;
 
 public static class SearchHelper
 {
+    public class SearchHighlight<TValue>
+    {
+        public SearchHighlight(TValue val)
+        {
+            Result = val;
+        }
+        public TValue Result { get; set; } = default!;
+        public List<string> HighlightedWords { get; set; } = new();
+    }
+    public static List<SearchHighlight<TValue>> LevenshteinSearch<TValue>(this IEnumerable<TValue> tIn, string searchTerm, Func<TValue, string> expression)
+    {
+        if (string.IsNullOrEmpty(searchTerm))
+            return (List<SearchHighlight<TValue>>)tIn.Select(x => new SearchHighlight<TValue>(x));
+
+        var first = true;
+        IEnumerable<TValue> results = null;
+        foreach (var s in searchTerm.ToSearchBreakout(false))
+        {
+            var numOfTypos = s.Length < 4 ? 1 : s.Length < 8 ? 2 : 3;
+
+            if (first)
+            {
+                results = tIn.Where(x => IsResult(expression(x), s, numOfTypos));
+            }
+            else
+            {
+                results = results.Where(x => IsResult(expression(x), s, numOfTypos));
+            }
+
+            first = false;
+        }
+
+        return new List<SearchHighlight<TValue>>();
+        //return results.ToList() ?? new List<TValue>();
+    }
+
+    private static bool IsResult(string strIn, string searchTerm, int typos)
+    {
+        if (string.IsNullOrEmpty(searchTerm))
+            return true;
+
+        var strSplit = strIn.Split(" ").Select(x => x.ToLower()).ToList();
+        if (strSplit.Any(x => x.StartsWith("protein")))
+        {
+            foreach (var s in strSplit)
+            {
+                var distance = Fastenshtein.Levenshtein.Distance(s.Substring(0, Math.Min(s.Length, searchTerm.Length)), searchTerm.ToLower());
+            }
+            var d = "";
+        }
+
+        return strSplit.Any(str =>
+            Fastenshtein.Levenshtein.Distance(str.Substring(0, Math.Min(str.Length, searchTerm.Length)), searchTerm.ToLower()) <= typos);
+
+    }
+
     /// <summary>
     /// Searched through a list and returns the items it finds
     /// </summary>
@@ -81,4 +141,5 @@ public static class SearchHelper
             yield return searchString.ToString();
         }
     }
+
 }
